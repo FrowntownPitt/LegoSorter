23eeb246a7419cb1b41ee8d879ee4691a85fdb90
Raphael Fernandes <raphael.cardoso.f@gmail.com>
2019-04-06 20:33:32 -0400

    Getting lego, need to crop and increase window box

--- main.py
+++ main.py
@@ -6,23 +6,14 @@ import numpy as np
 from keras.applications import InceptionV3
 import imutils
 	
-im = cv2.imread(os.path.abspath("photo3.jpg"))
+im = cv2.imread(os.path.abspath("photo2.jpg"))
 gray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
-# edges = cv2.Canny(img,100,200)
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
+canny = cv2.Canny(blurred, 20, 40)
+kernel = np.ones((5,5), np.uint8)
+dilated = cv2.dilate(canny, kernel, iterations=2)
+(contours, hierarchy) = cv2.findContours(dilated.copy(),cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+cnts = sorted(contours, key=cv2.contourArea, reverse=True)[:5]
 
-accumEdged = np.zeros(im.shape[:2], dtype="uint8")
-for chan in cv2.split(im):
-	chan = cv2.medianBlur(chan, 11)
-	edged = cv2.Canny(chan, 40, 180)
-	accumEdged = cv2.bitwise_or(accumEdged, edged)
-cv2.imwrite("res.jpg", accumEdged)
-cnts = cv2.findContours(accumEdged, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
-cnts = imutils.grab_contours(cnts)
-cnts = sorted(cnts, key=cv2.contourArea, reverse=True)[:5]
-for i in cnts:
-    peri = cv2.arcLength(i, True)
-    approx = cv2.approxPolyDP(i, 0.015 * peri, True)
-    print(len(approx))
-cv2.drawContours(im,cnts,-1,(0,255,0),3)
-cv2.imwrite("res.jpg", im)
+cv2.drawContours(im,cnts,0,(0,255,0),3)
+cv2.imwrite('res.jpg',im)
