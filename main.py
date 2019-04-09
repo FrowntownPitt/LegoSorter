7c7aa63e525eb6e35b49b35b804ae481f4d2d421
Raphael Fernandes <raphael.cardoso.f@gmail.com>
2019-04-09 18:35:12 -0400

    Testing trainment of ResNet50 with only 10 epochs

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import os
 from keras.preprocessing.image import img_to_array
 from preprocessing import PreProcessing	
 from neuralNetwork import NeuralNetwork
+from keras.preprocessing.image import ImageDataGenerator
 
 def createTrainImagesVectorAndLabels(pathToImageFolder):
     X_train = []
@@ -14,7 +15,7 @@ def createTrainImagesVectorAndLabels(pathToImageFolder):
             im = cv2.imread(imagePath,0)
             # if im.shape > highest:
                 # highest = cv2.imread(imagePath).shape
-            im = cv2.resize(im,(400,400))
+            # im = cv2.resize(im,(224,400))
             X_train.append(im)
             Y_train.append(folder)
     return X_train, Y_train
@@ -22,7 +23,11 @@ def createTrainImagesVectorAndLabels(pathToImageFolder):
 if __name__ == "__main__":
     # X_train, Y_train = createTrainImagesVectorAndLabels(os.path.abspath("real_Legos_images"))
     NN = NeuralNetwork()
-    NN.resNet50Model((224, 224, 3), 6)
+    gen = ImageDataGenerator(rotation_range=8, width_shift_range=0.08, shear_range=0.3,height_shift_range=0.08, zoom_range=0.08)
+    train_generator = gen.flow_from_directory(os.path.abspath(os.path.join("real_Legos_images")), target_size = (224,224), color_mode = "rgb", batch_size = 32, class_mode='categorical')
+    STEP_SIZE_TRAIN = train_generator.n//train_generator.batch_size
+    resNet = NN.resNet50Model((224, 224, 3), 6)
+    resNet.fit_generator(train_generator, steps_per_epoch = STEP_SIZE_TRAIN, epochs = 10)
     # NN.t()
     # preProcess = PreProcessing()
     # i = preProcess.cropPieceFromImage("rendered_LEGO-brick-images/train/3004 Brick 1x2/0001.png")
